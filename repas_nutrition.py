97834bdf59d29c2feba5b6ceb6db9bb3eed5fcd2
khechseck <cheikh.seck@ecoles-epsi.net>
2026-04-07 11:34:58 +0200

    feat: ajout du module repas avec 3 objectifs et conseils nutrition

---
+++ repas_nutrition.py
@@ -0,0 +1,3 @@
+git branch -m "page sur les fonctionnalités/la nutrition" feat/repas-nutrition
+git push -u origin feat/repas-nutrition
+git push origin --delete "page sur les fonctionnalités/la nutrition"

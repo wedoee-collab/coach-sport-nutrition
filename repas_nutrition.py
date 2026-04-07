428aa79fb4c14c3fb33baf890d7a6ed7f284faf4
khechseck <cheikh.seck@ecoles-epsi.net>
2026-04-07 11:41:40 +0200

    fix: suppression du fichier incorrect

--- repas_nutrition.py
+++
@@ -1,3 +0,0 @@
-git branch -m "page sur les fonctionnalités/la nutrition" feat/repas-nutrition
-git push -u origin feat/repas-nutrition
-git push origin --delete "page sur les fonctionnalités/la nutrition"
